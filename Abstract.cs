22f978240742bf0bae9683823e28df487996546f
Кравченко Дмитро <cravchenko5@ukr.net>
2020-11-22 20:06:18 +0200

    Добавьте файлы проекта.

---
+++ Abstract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace KravchenkoD2_12FIT6
+{
+	abstract class AbstractHandler
+	{
+		public abstract void Open(string path);
+		public abstract void Create(string path);
+		public abstract void Edit(params object[] settings);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KravchenkoD2_12FIT6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
